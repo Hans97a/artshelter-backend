005a7efee6414eb0ba76f8761626baeeb9f4ecbb
bokyeal kim <byby8992@gmail.com>
2023-02-27 21:23:03 +0900

    first commit

--- config/settings.py
+++ config/settings.py
@@ -106,10 +106,10 @@ else:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
-            "HOST": os.environ.get("RDS_HOST"),
-            "NAME": os.environ.get("RDS_NAME"),
-            "USER": os.environ.get("RDS_USER"),
-            "PASSWORD": os.environ.get("RDS_PASSWORD"),
+            "HOST": env.str("RDS_HOST"),
+            "NAME": env.str("RDS_NAME"),
+            "USER": env.str("RDS_USER"),
+            "PASSWORD": env.str("RDS_PASSWORD"),
             "PORT": "5432",
         }
     }
