3c84635b924bbc2e85773d337d7796654b65322d
bokyeal kim <byby8992@gmail.com>
2023-03-13 05:43:52 +0900

    delete annotation

--- edus/models.py
+++ edus/models.py
@@ -11,7 +11,6 @@ class Education(TimeStampedModel):
         blank=False, upload_to=upload_to("edus", True), verbose_name="대표 이미지"
     )
     blog_url = models.CharField(max_length=250, verbose_name="블로그 링크")
-    # content = models.TextField(default="", verbose_name="본문")
 
     def __str__(self):
         return self.title
